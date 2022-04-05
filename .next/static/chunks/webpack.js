451bc6873354c7f5c786e828617340ab0fb7bf4c
Hexdee <lukmansaheed79@gmail.com>
2022-04-05 07:09:39 +0100

    fixed mobile disply

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "39dc7a38c4bf609e"; }
+/******/ 		__webpack_require__.h = function() { return "59cf67658dc64b7c"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
